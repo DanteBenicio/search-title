e50ad1dd0dd683ff18968b3b4b60301381dbddc8
DanteBenicio <dantecosta79@gmail.com>
2022-02-11 12:25:07 -0300

    input group component and posts component

--- src/App.tsx
+++ src/App.tsx
@@ -27,40 +27,21 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <section className="section_container">
+      <div className="input_group">
+        <label htmlFor="input-search">Buscar Titulo</label>
+        <input type="text" id="input-search" />
+      </div>
+      <main className="posts_container">
+        {posts.map((post) => (
+          <article className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </article>
+        ))}
+      </main>
+    </section>
+  );
 }
 
 export default App
